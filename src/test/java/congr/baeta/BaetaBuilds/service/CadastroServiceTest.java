24b04ef774560cd4248f8c930ff1513e0eb5cf0e
erick-augusto <erick_aug@hotmail.com>
2025-05-14 20:24:04 -0300

    Criando classe de testes do serviço de cadastro

---
+++ src/test/java/congr/baeta/BaetaBuilds/service/CadastroServiceTest.java
@@ -0,0 +1,50 @@
+package congr.baeta.BaetaBuilds.service;
+
+import org.junit.jupiter.api.Test;
+
+public class CadastroServiceTest {
+    @Test
+    void testAddAptoTerritorio() {
+
+    }
+
+    @Test
+    void testAddAptoTorre() {
+
+    }
+
+    @Test
+    void testCadTerrirorio() {
+
+    }
+
+    @Test
+    void testCadastrarApto() {
+
+    }
+
+    @Test
+    void testCadastroTorre() {
+
+    }
+
+    @Test
+    void testFindApto() {
+
+    }
+
+    @Test
+    void testFindTerritorioById() {
+
+    }
+
+    @Test
+    void testFindTorreById() {
+
+    }
+
+    @Test
+    void testSalvarApto() {
+
+    }
+}
